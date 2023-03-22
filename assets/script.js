5405fcc8b0f12013d4a31ec8e5df4e043206d47a
ClaireMorris89 <124100820+ClaireMorris89@users.noreply.github.com>
2023-03-22 17:06:51 -0600

    added new html fieldset question 1

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,155 @@
-var timer = document.querySelector('#timer');
-var startButton = document.querySelector('.start-btn');
-var questionSelector = 0;
-var answerButton=document.querySelector('.answer');
-var timeLeft=10;
-
-var questions = [
-    {
-        question:"question 1",  
-        answer1:"answer a",
-        answer2:"answer b",
-        answer3:"answer c",
-        answer4:"answer d",
-        correct:"answer a",
-    },
-    {
-        question:"question 2",  
-        answer1:"answer a",
-        answer2:"answer b",
-        answer3:"answer c",
-        answer4:"answer d",
-        correct:"answer a",
-    },
-    {
-        question:"question 3",  
-        answer1:"answer a",
-        answer2:"answer b",
-        answer3:"answer c",
-        answer4:"answer d",
-        correct:"answer a",
-    },
-];
-
-//how to make sure it runs next question/answer set? increment questionSelector?
-function questionsPopulator(){
-    document.getElementById('question').textContent=questions[questionSelector].question;
-    document.getElementById('op1').textContent=questions[questionSelector].answer1;
-    document.getElementById('op2').textContent=questions[questionSelector].answer2;
-    document.getElementById('op3').textContent=questions[questionSelector].answer3;
-    document.getElementById('op4').textContent=questions[questionSelector].answer4;
-};
 
 
-//get help on how to input info from selected button
-function evaluateAnswer() {
-    if (answerButton.disabled === questions.correct){
-    questionSelector++; //(increment questionSelector to run next question)
-    questionsPopulator();
-}  else {
-    timeLeft--; //subtract time for being wrong(how to subtract 3 seconds??)
-    questionSelector++;//(increment questionSelector to run next question)
-    questionsPopulator();
-}
-};
 
 
-function countdown(){
-    var timeInterval = setInterval (function (){
-        if (timeLeft > 1) {
-            timer.textContent = timeLeft + ' seconds left';
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            timer.textContent = timeLeft + ' second left';
-            timeLeft--;
-        } else {
-            timer.textContent = 'time is up!';
-            clearInterval(timeInterval);
-        }
 
-        }, 1000);
-    };
 
-    console.log(questions);
 
-    function startQuiz(){
-        countdown();
-        questionsPopulator();
-    }
 
-startButton.addEventListener("click", startQuiz);
-answerButton.addEventListener("click",evaluateAnswer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var timer = document.querySelector('#timer');
+// var startButton = document.querySelector('.start-btn');
+// var questionSelector = 0;
+// var timeLeft=50;
+// var score = 0;
+
+// //var buttonEl = document.getElementsByClassName('answer');
+// // var answerA = document.getElementById("op1");
+// // var answerB = document.getElementById("op2");
+// // var answerC = document.getElementById("op3");
+// // var answerD = document.getElementById("op4");
+
+// //should be queryselectorAll but doesn't work with event listener
+// //var answerButton=document.querySelector('.answer');
+// // var answerButtons=document.querySelectorAll('.answer');
+
+// var questions = [
+//     {
+//         question:"question 1",  
+//         answer1:"answer a",
+//         answer2:"answer b",
+//         answer3:"answer c",
+//         answer4:"answer d",
+//         correct:"answer a",
+//     },
+//     {
+//         question:"question 2",  
+//         answer1:"answer a",
+//         answer2:"answer b",
+//         answer3:"answer c",
+//         answer4:"answer d",
+//         correct:"answer b",
+//     },
+//     {
+//         question:"question 3",  
+//         answer1:"answer a",
+//         answer2:"answer b",
+//         answer3:"answer c",
+//         answer4:"answer d",
+//         correct:"answer c",
+//     },
+//     {
+//         question:"question 4",  
+//         answer1:"answer a",
+//         answer2:"answer b",
+//         answer3:"answer c",
+//         answer4:"answer d",
+//         correct:"answer c",
+//     },
+//     {
+//         question:"question 5",  
+//         answer1:"answer a",
+//         answer2:"answer b",
+//         answer3:"answer c",
+//         answer4:"answer d",
+//         correct:"answer c",
+//     },
+// ];
+
+
+// //how to make sure it runs next question/answer set? increment questionSelector?
+// function questionsPopulator(){
+//     document.getElementById('question').textContent=questions[0].question;
+//     document.getElementById('op1').value=questions[0].answer1;
+//     document.getElementById('op2').value=questions[0].answer2;
+//     document.getElementById('op3').value=questions[0].answer3;
+//     document.getElementById('op4').value=questions[0].answer4;
+// };
+
+
+// //get help on how to input info from selected button
+// function evaluateAnswer(event){
+//    var buttonEl = event.target;
+
+//    if (buttonEl.value = questions.question[0].correct){
+//     console.log('correct');
+//    } else {
+//     console.log('wrong');
+//    }
+   
+    
+// };
+
+// // how to stop it when it runs out of questions to pull??
+
+// function countdown(){
+//     var timeInterval = setInterval (function (){
+//         if (timeLeft > 1) {
+//             timer.textContent = timeLeft + ' seconds left';
+//             timeLeft--;
+//         } else if (timeLeft === 1) {
+//             timer.textContent = timeLeft + ' second left';
+//             timeLeft--;
+//         } else {
+//             timer.textContent = 'time is up!';
+//             clearInterval(timeInterval);
+//         }
+
+//         }, 1000);
+//     };
+
+
+//     function startQuiz(){
+//         countdown();
+//         questionsPopulator();
+//         };
+
+
+// startButton.addEventListener("click", startQuiz);
+
+//adding eventlistener to each button???
+/*document.querySelectorAll('.answer').forEach(answer =>
+    answer.addEventListener('click', evaluateAnswer)
+    );*/
+
+ //doesn't work with queryselectorALl
+//answerButtons.addEventListener("click", function (){  }); 
+
+// answerA.addEventListener("click",evaluateAnswer);
+// answerB.addEventListener("click",evaluateAnswer);
+// answerC.addEventListener("click",evaluateAnswer);
+// answerD.addEventListener("click",evaluateAnswer);
+
+// buttonEl.addEventListener('click',evaluateAnswer);
